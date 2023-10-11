ee5448256070497bb0a40eabc442cc53a8541e5f
PapelConPera <lionel.copajira@gmail.com>
2023-10-11 10:56:45 -0300

    modificando la act 4

---
+++ appPeliculas/pelis.py
@@ -0,0 +1,18 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from movies.db import get_db
+
+bp = Blueprint('pelis', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    pelis = db.execute(
+        """SELECT l.name AS lenguaje, f.title AS titulo
+           FROM language l JOIN film f ON l.language_id = f.language_id
+           ORDER BY name ASC"""
+    ).fetchall()
+    return render_template('pelis/index.html', pelis=pelis)
